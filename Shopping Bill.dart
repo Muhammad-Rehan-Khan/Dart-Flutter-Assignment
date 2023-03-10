407dc285bff26d0b3406318c3b43541038e11cbf
Rehan Achakzai <126003639+RehanAchakxaii@users.noreply.github.com>
2023-03-10 10:09:20 -0800

    Add files via upload
    Practice

---
+++ Shopping Bill.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print("GROCCERY BILL");
+
+  double appleprice = 8.9;
+  double bananaprice = 5.9;
+  double carrotprice = 5.9;
+
+  print("Enter apple quantity");
+  int appleQuantity = int.parse(stdin.readLineSync()!);
+  var p = appleprice * appleQuantity;
+  print("enter banana price");
+  int bananaquantity = int.parse(stdin.readLineSync()!);
+  var b = bananaprice * bananaquantity;
+  print("enter carrot quantity");
+  int carrotqauantity = int.parse(stdin.readLineSync()!);
+  var c = carrotprice * carrotqauantity;
+
+  var t = p + b + c;
+  print("your Total bill is $t /= USD");
+}
